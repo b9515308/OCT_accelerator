6cd6ce2808c169aea7e8886e019d9e74f8469079
lucas <b9515038@gmail.com>
2018-06-07 00:38:28 +0800

    output source correctly

--- oct_acc_cpu.cpp
+++ oct_acc_cpu.cpp
@@ -9,26 +9,65 @@
  *
  */
 
-////////////////////////////////////////////////////////////////////////////////
-// export C interface
-extern "C"
-void computeGold(float *reference, float *idata, const unsigned int len);
+//prepare_data()
+//data_calculation()
+//disaply()
 
-////////////////////////////////////////////////////////////////////////////////
-//! Compute reference data set
-//! Each element is multiplied with the number of threads / array length
-//! @param reference  reference data, computed but preallocated
-//! @param idata      input data as provided to device
-//! @param len        number of elements in reference / idata
-////////////////////////////////////////////////////////////////////////////////
-void
-computeGold(float *reference, float *idata, const unsigned int len)
+#include <opencv2/opencv.hpp>
+#include <stdio.h>
+#define F_ROW (2048)
+#define F_COL (3000)
+#define F_SZ (F_ROW*F_COL)
+#define DEPTH (sizeof(unsigned short))
+#define SPEC_SZ (2048 * sizeof(float))
+using namespace cv;
+
+void write_raw_image(void *raw_image,const char *filename, unsigned int size)
 {
-    const float f_len = static_cast<float>(len);
+    FILE *out_fp;
+    out_fp = fopen(filename, "wb");
+    fwrite(raw_image,1,size,out_fp);
+    fclose(out_fp);
 
-    for (unsigned int i = 0; i < len; ++i)
-    {
-        reference[i] = idata[i] * f_len;
-    }
 }
 
+
+int main()
+{
+	FILE *fp;
+	void *frame;
+	void *bg_frame;
+	float *spec;
+
+	frame = malloc(F_SZ * DEPTH);
+	fp = fopen("resource/input_graph/frame13508.2048.SDOCT.dat","rb");
+	fread(frame, DEPTH, F_SZ, fp);
+	fclose(fp);
+
+
+	bg_frame = malloc(F_SZ * DEPTH);
+	fp = fopen("resource/bg_files/frame00234.2048.SDOCT.dat", "rb");
+	fread(bg_frame, DEPTH, F_SZ, fp);
+	fclose(fp);
+
+	
+	spec = (float *)malloc(SPEC_SZ);
+	fp = fopen("resource/spectrum/frame13508.spectrum", "r");
+	for(int i; i < 2048; i++)
+		fscanf(fp, "%f", &spec[i]);
+	fclose(fp);
+
+	Mat mat_frame(F_ROW, F_COL, CV_8U, frame);
+	write_raw_image(mat_frame.ptr(), "2048x3000_SRC", 2048*3000*2 );
+	namedWindow("OCT");
+	imshow("OCT", mat_frame);
+
+	waitKey(0);
+
+	destroyWindow("OCT");
+	mat_frame.release();
+	free(frame);
+	free(spec);
+	free(bg_frame);
+	return 0;
+}
